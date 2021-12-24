d428d67c32af6ed17b6a7c70ec0f01e4845ce7cb
AY <a@qx.zone>
2021-12-24 20:43:21 +0200

    Add missing config.h

---
+++ pjproject-1.8.10/third_party/bcg729/src/config.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+* config.h.cmake
+* Copyright (C) 2014  Belledonne Communications, Grenoble France
+*
+****************************************************************************
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*
+****************************************************************************/
+
+#define PACKAGE "bcg729"
+#define PACKAGE_NAME "bcg729"
+#define PACKAGE_VERSION "1.1.1"
+#define PACKAGE_STRING "bcg729 1.1.1"
+#define PACKAGE_TARNAME "bcg729"
+#define PACKAGE_BUGREPORT "support@belledonne-communications.com"
+/* #undef PACKAGE_URL */
+#define VERSION "1.1.1"
+
+//#define BCG729_STATIC
